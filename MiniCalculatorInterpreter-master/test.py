c8ec070676e4f3217774e6bf411f712ac9dc9e51
AGUIR Chams <109348212+AguirChams@users.noreply.github.com>
2024-04-30 23:36:23 +0200

    Update test.py

--- MiniCalculatorInterpreter-master/test.py
+++ MiniCalculatorInterpreter-master/test.py
@@ -1,26 +1,22 @@
 import unittest
-from calculator import p_expression_binop  # Replace 'your_module' with the actual module name where your function is defined
+from calculator import addition  # Replace 'your_module' with the actual module name where your function is defined
+class TestAddition(unittest.TestCase):
+    def test_addition_positive_numbers(self):
+        result = addition(3, 5)
+        self.assertEqual(result, 8)
 
-class TestExpression(unittest.TestCase):
-    def test_addition(self):
-        t = [1, '+', 2]
-        p_expression_binop(t)
-        self.assertEqual(t[0], 3)
+    def test_addition_negative_numbers(self):
+        result = addition(-3, -5)
+        self.assertEqual(result, -8)
 
-    def test_subtraction(self):
-        t = [5, '-', 3]
-        p_expression_binop(t)
-        self.assertEqual(t[0], 2)
+    def test_addition_mixed_numbers(self):
+        result = addition(3, -5)
+        self.assertEqual(result, -2)
 
-    def test_multiplication(self):
-        t = [4, '*', 6]
-        p_expression_binop(t)
-        self.assertEqual(t[0], 24)
+    def test_addition_zero(self):
+        result = addition(0, 5)
+        self.assertEqual(result, 5)
 
-    def test_division(self):
-        t = [10, '/', 2]
-        p_expression_binop(t)
-        self.assertEqual(t[0], 5)
 
 if __name__ == '__main__':
     unittest.main()
